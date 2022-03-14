7a540a80d09af6565a90683889c8516f52cbec2d
Victor Z. Shi <vzshi@pm.me>
2022-03-14 01:29:14 -0400

    Refactor Planets

--- Game/src/Scene/Pools/Planets/Planets.h
+++ Game/src/Scene/Pools/Planets/Planets.h
@@ -2,21 +2,15 @@
 
 #include "Scene/Pools/Pool.h"
 
-#include <set>
-
 struct Vector3;
 
 class Planets : public Pool
 {
   public:
-    const int TOTAL = 5;
-
     virtual void Init(Scene &scene) override;
 
     virtual void Update(Scene &scene) override;
 
   private:
-    std::set<int> m_random;
-
     int CreatePlanet(Scene &scene, Vector3 &position);
 };
